8a7b9245ee4a57f8807956a47bbdbf7a9db8ea04
emrgnt-cmplxty <68796651+emrgnt-cmplxty@users.noreply.github.com>
2023-10-05 08:48:38 -0400

    add ability to remix data quickly (#36)
    Co-authored-by: EC2 Default User <ec2-user@ip-172-31-17-95.ec2.internal>

---
+++ smol_trainer/data/data_config.py
@@ -0,0 +1,111 @@
+# datasets_config.py
+
+datasets_config = {
+    "sciphi-python-textbook": {
+        "path": "emrgnt-cmplxty/sciphi-python-textbook",
+        "mappings": {
+            "formatted_prompt": "### Instruction:",
+            "completion": "### Response:",
+        },
+        "tokenizer": "simple_tokenize",
+    },
+    
+    "sciphi-textbooks-are-all-you-need": {
+        "path": "emrgnt-cmplxty/sciphi-textbooks-are-all-you-need",
+        "mappings": {
+            "formatted_prompt": "### Instruction:",
+            "completion": "### Response:",
+        },
+        "tokenizer": "simple_tokenize",
+    },
+
+    "open-phi-textbooks": {
+        "path": "open-phi/textbooks",
+        "mappings": {
+            "markdown": ""
+        },
+        "tokenizer": "simple_tokenize",
+    },
+
+    "programming-books-llama": {
+        "path": "open-phi/programming_books_llama",
+        "mappings": {
+            "markdown": ""
+        },
+        "tokenizer": "simple_tokenize",
+    },
+
+    "open-orca": {
+        "path": "Open-Orca/OpenOrca",
+        "mappings": {
+            "system_prompt": "### System:",
+            "question": "### Question:",
+            "response": "### Response:",
+        },
+        "tokenizer": "simple_tokenize",
+    },
+
+    "tiny-stories": {
+        "path": "roneneldan/TinyStories",
+        "mappings": {
+            "text": ""
+        },
+        "tokenizer": "simple_tokenize",
+    },
+
+    "tiny-codes": {
+        "path": "nampdn-ai/tiny-codes",
+        "mappings": {
+            "prompt": "### Instruction:",
+            "response": "### Response:",
+        },
+        "tokenizer": "simple_tokenize",
+    },
+
+    "tiny-orca": {
+        "path": "nampdn-ai/tiny-orca-textbooks",
+        "mappings": {
+            "prompt": "### System:",
+            "question": "### Question:",
+            "textbook": "### Context:",
+            "response": "### Response:",
+        },
+        "tokenizer": "simple_tokenize",
+    },
+
+    "tiny-textbooks": {
+        "path": "nampdn-ai/tiny-textbooks",
+        "mappings": {
+            "text": "### Instruction:\nWrite a lesson based on the following content:",
+            "textbook": "### Response:",
+        },
+        "tokenizer": "simple_tokenize",
+    },
+
+    "meta-math": {
+        "path": "meta-math/MetaMathQA",
+        "mappings": {
+            "query": "### Question:",
+            "response": "### Answer:",
+        },
+        "tokenizer": "simple_tokenize",
+    },
+
+    "evol-instruct": {
+        "path": "nickrosh/Evol-Instruct-Code-80k-v1",
+        "mappings": {
+            "instruction": "### Instruction:",
+            "output": "### Output:",
+        },
+        "tokenizer": "simple_tokenize",
+    },
+
+    "open-platypus": {
+        "path": "garage-bAInd/Open-Platypus",
+        "mappings": {
+            "instruction": "### Instruction:",
+            "output": "### Output:",
+        },
+        "tokenizer": "simple_tokenize",
+    }
+}
